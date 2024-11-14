db4c4184c5347df486378d470fe1a14f3559d746
Sakshijain-Get <76477918+Sakshijain-Get@users.noreply.github.com>
2024-11-14 19:23:34 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/facebookresearch/segment-anything-2.git
+cd segment-anything-2
+pip install -e .
+
+# Create checkpoints folder and download SAM2 model weights
+mkdir -p /app/checkpoints
+wget -q https://dl.fbaipublicfiles.com/segment_anything_2/072824/sam2_hiera_tiny.pt -P /app/checkpoints
+wget -q https://dl.fbaipublicfiles.com/segment_anything_2/072824/sam2_hiera_small.pt -P /app/checkpoints
+wget -q https://dl.fbaipublicfiles.com/segment_anything_2/072824/sam2_hiera_base_plus.pt -P /app/checkpoints
+wget -q https://dl.fbaipublicfiles.com/segment_anything_2/072824/sam2_hiera_large.pt -P /app/checkpoints
